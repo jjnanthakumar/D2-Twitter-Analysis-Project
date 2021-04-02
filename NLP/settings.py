4164adba1ed90da639aba8b8e9f770ee6a14c03a
Nanthakumar J J <jjnanthakumar477@gmail.com>
2021-04-02 10:37:42 +0530

    Updated

--- NLP/settings.py
+++ NLP/settings.py
@@ -80,7 +80,7 @@ SOCIALACCOUNT_PROVIDERS = {
     },
     'linkedin_oauth2': {
         'SCOPE': [
-            'r_basicprofile',
+            'r_liteprofile',
             'r_emailaddress'
         ],
         'PROFILE_FIELDS': [
